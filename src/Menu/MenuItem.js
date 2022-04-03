d6469c36acba0738a6444348248d996850e490d7
niakate <mamadouniakate382@gmail.com>
2022-04-03 20:09:00 +0200

    manage anchor links

--- src/Menu/MenuItem.js
+++ src/Menu/MenuItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 // import './styles.css';
 
 import { motion } from 'framer-motion'
