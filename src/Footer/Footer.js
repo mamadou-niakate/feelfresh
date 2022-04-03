6fdbf6f6e0e770e69e6bd4e6d29a9974156984ad
niakate <mamadouniakate382@gmail.com>
2022-04-03 21:40:08 +0200

    refactoring

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { Divider, Grid } from '@mui/material'
 import { makeStyles } from '@mui/styles';
 import { Typography } from '@mui/material'
 import { FaSnapchat, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
@@ -56,63 +56,65 @@ const useStyles = makeStyles({
 const Footer = () => {
     const classes = useStyles()
     return (
-        <Grid container justifyContent={'center'} alignItems='flex-start' className={classes.root}>
-            <Grid item xs={12} md={4} style={{ display:'flex', flexDirection:'column'}}>
-                <Typography variant='h4' className={classes.footerSectionTitle}>
-                    Suivez-nous sur les réseaux sociaux
-                </Typography>
-                <div className={classes.socialNetwork}>
-                    <Typography component={'h6'} variant='h6'>
-                        <a href="https://www.facebook.com/" className={classes.link} target='_blank' rel='noreferrer'>
-                            <FaFacebook  className={classes.socialNetworkIcon}/>
-                            <span> Facebook </span>
-                        </a>
+        <div className={classes.root}>
+            <Grid container justifyContent={'center'} alignItems='flex-start'>
+                <Grid item xs={12} md={4} style={{ display:'flex', flexDirection:'column'}}>
+                    <Typography variant='h4' className={classes.footerSectionTitle}>
+                        Suivez-nous sur les réseaux sociaux
                     </Typography>
-                    <Typography component={'h6'} variant='h6'>
-                        <a href="https://www.instagram.com/" className={classes.link} target='_blank' rel='noreferrer'>
-                            <FaInstagram className={classes.socialNetworkIcon}/>
-                            <span> Instagram </span>
-                        </a>
+                    <div className={classes.socialNetwork}>
+                        <Typography component={'h6'} variant='h6'>
+                            <a href="https://www.facebook.com/" className={classes.link} target='_blank' rel='noreferrer'>
+                                <FaFacebook  className={classes.socialNetworkIcon}/>
+                                <span> Facebook </span>
+                            </a>
+                        </Typography>
+                        <Typography component={'h6'} variant='h6'>
+                            <a href="https://www.instagram.com/" className={classes.link} target='_blank' rel='noreferrer'>
+                                <FaInstagram className={classes.socialNetworkIcon}/>
+                                <span> Instagram </span>
+                            </a>
+                        </Typography>
+                        <Typography component={'h6'} variant='h6'>
+                            <a href="https://www.twitter.com/" className={classes.link} target='_blank' rel='noreferrer'>
+                                <FaTwitter className={classes.socialNetworkIcon}/>
+                                <span> Twitter </span>
+                            </a>
+                        </Typography>
+                        <Typography component={'h6'} variant='h6'>
+                            <a href="https://wwww.snapchat.com/" className={classes.link} target='_blank' rel='noreferrer'>
+                                <FaSnapchat className={classes.socialNetworkIcon}/>
+                                <span> Snapchat </span>
+                            </a>
+                        </Typography>
+                    </div>
+                </Grid>
+                <Grid item xs={12} md={4} className={classes.infoUtil}>
+                    <Typography variant='h4' className={classes.footerSectionTitle}>
+                        Infos Utils
                     </Typography>
-                    <Typography component={'h6'} variant='h6'>
-                        <a href="https://www.twitter.com/" className={classes.link} target='_blank' rel='noreferrer'>
-                            <FaTwitter className={classes.socialNetworkIcon}/>
-                            <span> Twitter </span>
-                        </a>
+                    <Typography className={classes.text} component='h6' variant='h6'> Témoignages de nos clients  </Typography>
+                    <Typography className={classes.text} component='h6' variant='h6'>  A Propos de nous </Typography>
+                    <Typography className={classes.text} component='h6' variant='h6'> Nos Plats </Typography>
+                    <Typography className={classes.text} component='h6' variant='h6'> Contacter le développeur </Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <Typography variant='h4' className={classes.footerSectionTitle}>
+                        Où Nous Trouver  ?
                     </Typography>
-                    <Typography component={'h6'} variant='h6'>
-                        <a href="https://wwww.snapchat.com/" className={classes.link} target='_blank' rel='noreferrer'>
-                            <FaSnapchat className={classes.socialNetworkIcon}/>
-                            <span> Snapchat </span>
-                        </a>
+                    <Typography className={classes.text} component='h6' variant='h6'> 
+                        <strong>Adresse : </strong>  
+                        <span>Rue 413 Entre Le Palais Des Sports Et La Place Can Proche De Guaduman </span>
                     </Typography>
-                </div>
+                    <Typography className={classes.text} component='h6' variant='h6'> 
+                        <strong>Horraire : </strong>  <br/>
+                        Lundi - Vendredi : 9:00 - 18:00 <br/>
+                        Samedi : 9:00 - 14:00 <br/>
+                        Dimanche : Fermé
+                    </Typography>
+                </Grid>
             </Grid>
-            <Grid item xs={12} md={4} className={classes.infoUtil}>
-                <Typography variant='h4' className={classes.footerSectionTitle}>
-                    Infos Utils
-                </Typography>
-                <Typography className={classes.text} component='h6' variant='h6'> Témoignages de nos clients  </Typography>
-                <Typography className={classes.text} component='h6' variant='h6'>  A Propos de nous </Typography>
-                <Typography className={classes.text} component='h6' variant='h6'> Nos Plats </Typography>
-                <Typography className={classes.text} component='h6' variant='h6'> Contacter le développeur </Typography>
-            </Grid>
-            <Grid item xs={12} md={4}>
-                <Typography variant='h4' className={classes.footerSectionTitle}>
-                    Où Nous Trouver  ?
-                </Typography>
-                <Typography className={classes.text} component='h6' variant='h6'> 
-                    <strong>Adresse : </strong>  
-                    <span>Rue 413 Entre Le Palais Des Sports Et La Place Can Proche De Guaduman </span>
-                </Typography>
-                <Typography className={classes.text} component='h6' variant='h6'> 
-                    <strong>Horraire : </strong>  <br/>
-                    Lundi - Vendredi : 9:00 - 18:00 <br/>
-                    Samedi : 9:00 - 14:00 <br/>
-                    Dimanche : Fermé
-                </Typography>
-            </Grid>
-        </Grid>
+        </div>
     )
 }
 
